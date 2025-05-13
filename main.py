859684da1c46b24bca79b895259df8229c560277
Agramm18 <grammaleks@gmail.com>
2025-05-13 19:27:09 +0200

    comments_where_added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Basic imports for the required PyQt6 libraries and system access
 from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 from PyQt6.QtCore import Qt, QSize
 import sys
@@ -5,20 +6,23 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Resumee Analyzer")
+
+        # Configure main window
+        self.setWindowTitle("Resume Analyzer")
         self.setFixedSize(QSize(500, 500))
 
-        widget = QLabel("Drag the resumee here")
-        font = widget.font()
+        # Create a basic label with user instructions
+        label = QLabel("Drag the resume here")
+        font = label.font()
         font.setPointSize(15)
-        widget.setFont(font)
-        widget.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-        widget.setContentsMargins(0, 20, 0, 0)
+        label.setFont(font)
+        label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        label.setContentsMargins(0, 20, 0, 0)
 
-        self.setCentralWidget(widget)
+        self.setCentralWidget(label)
 
+# Application setup and launch
 app = QApplication(sys.argv)
-window = QWidget()
 window = MainWindow()
 window.show()
 app.exec()
