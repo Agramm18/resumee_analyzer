dcc290872a0b9a1c7a5dbfa3bd1e88cf304ae6d2
Agramm18 <grammaleks@gmail.com>
2025-05-17 22:45:44 +0200

    basic_layout_where_coded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Basic imports for the required PyQt6 libraries and system access
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QVBoxLayout, QLineEdit
 from PyQt6.QtCore import Qt, QSize
 import sys
 
@@ -11,15 +11,49 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Resume Analyzer")
         self.setFixedSize(QSize(500, 500))
 
-        # Create a basic label with user instructions
-        label = QLabel("Drag the resume here")
-        font = label.font()
-        font.setPointSize(15)
-        label.setFont(font)
-        label.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-        label.setContentsMargins(0, 20, 0, 0)
+        central_widget = QWidget()
+        layout = QVBoxLayout()
 
-        self.setCentralWidget(label)
+        #Welcome text
+        gui_title = QLabel("Welcome to my resumee analyzer")
+        font = gui_title.font()
+        font.setPointSize(20)
+        gui_title.setFont(font)
+        gui_title.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        gui_title.setContentsMargins(0, 5, 0, 0)
+        layout.addWidget(gui_title)
+
+        #input field for job advertisment
+        job_advertisment = QLineEdit(parent=self)
+        layout.addWidget(job_advertisment)
+
+        sub_text = QLabel("Please paste in a job advertisement")
+        font = sub_text.font()
+        font.setPointSize(15)
+        sub_text.setFont(font)
+        sub_text.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        sub_text.setContentsMargins(0, 10, 0, 0)
+        layout.addWidget(sub_text)
+        
+        # drag & drop field for the resume
+        label_title = QLabel("Drag the resume here")
+        font = label_title.font()
+        font.setPointSize(15)
+        label_title.setFont(font)
+        label_title.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+        label_title.setContentsMargins(0, 10, 0, 0)
+        label_title.setStyleSheet("""
+                        QLabel {
+                                  border: 2px solid white;
+                                  border-radius: 25px;
+                                  padding: 25px;
+
+                                  }
+                                  """)
+        layout.addWidget(label_title)
+
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
 # Application setup and launch
 app = QApplication(sys.argv)
