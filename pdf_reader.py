49da6d237b53a903ee155aca2af410d016e002d3
Agramm18 <grammaleks@gmail.com>
2025-05-22 20:36:41 +0200

    shematic-changes-where-coded

--- pdf_reader.py
+++ pdf_reader.py
@@ -5,7 +5,7 @@ class pdfData:
         self.data = []
         self.pdf_text = None
 
-    def read_pdf_data(self, filepath="resume.pdf"):
+    def read_pdf_file(self, filepath="resume.pdf"):
        if filepath is None:
           raise ValueError("Please provide a pdf file")
        
@@ -19,16 +19,14 @@ class pdfData:
 
                 self.pdf_text = text
        
-    def get_data(self):
+    def get_pdf(self):
         if self.pdf_text is None:
             raise ValueError("No PDF text to extract data from.")
 
-        # Beispiel: Füge Text zur Datenliste hinzu
         self.data.append(self.pdf_text)
         return self.data
-    
-if __name__ == "__main__":
-    pdf_reader = pdfData()
-    pdf_reader.read_pdf_data("resume.pdf")
-    extracted_data = pdf_reader.get_data()
-    print(extracted_data)
+
+pdf_reader = pdfData()
+pdf_reader.read_pdf_file("resume.pdf")
+extracted_data = pdf_reader.get_pdf()
+print(extracted_data)
