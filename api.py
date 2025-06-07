9da346585552d2dfa06fa51dfd054ac4bff46e2a
Agramm18 <grammaleks@gmail.com>
2025-06-08 00:56:51 +0200

    base-for-api-coded-and-bad-code-remooved

--- api.py
+++ api.py
@@ -5,9 +5,21 @@
 
 from main import MainWindow
 
-final_data = MainWindow.collect_final_data
+class getData:
+    def __init__(self, res_text, job_adv):
+        self.res_text = res_text
+        self.job_adv = job_adv
+    
+    def store_pdf(self):
+        if self.res_text:
+            print(f"The extracted text from the resume is: {self.res_text} \n")
+        
+        else:
+            raise ValueError("There is no resume")
 
+    def store_job_adv(self):
+        if self.job_adv:
+            print(f"The job advertisment is: {self.job_adv}")
 
-class dataCollector:
-    def __init__(self, job_advertisment=final_data):
-        self.job_advertisment = job_advertisment
+        else:
+            raise ValueError("There is no job advertisment")
