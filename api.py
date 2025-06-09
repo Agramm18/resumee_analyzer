e7e0d7dd30d2f28c3126df78a7a4c220fe626cce
Agramm18 <grammaleks@gmail.com>
2025-06-09 14:50:42 +0200

    added-language-detection-for-the-pdf

--- api.py
+++ api.py
@@ -3,23 +3,25 @@
 #and send it to the other codes
 #so the performance is better
 
+#Spacy imports
+import spacy
+import en_core_web_trf
+import de_dep_news_trf
+import spacy_fastlang
+
+#GUI imports
 from main import MainWindow
 
+#NLP Code imports
+from resume_nlp import collectDocument
+
+#values from the gui
 class getData:
     def __init__(self, res_text, job_adv):
         self.res_text = res_text
         self.job_adv = job_adv
-    
-    def store_pdf(self):
-        if self.res_text:
-            print(f"The extracted text from the resume is: {self.res_text} \n")
+
+class languageDetection:
+    def __init__(self, language):
+        self.resume_language = language
         
-        else:
-            raise ValueError("There is no resume")
-
-    def store_job_adv(self):
-        if self.job_adv:
-            print(f"The job advertisment is: {self.job_adv}")
-
-        else:
-            raise ValueError("There is no job advertisment")
